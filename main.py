a39194193d1be846a2da19e202abc74111bfd2da
Marcelo Nogueira <66036697+lethalarms23@users.noreply.github.com>
2021-03-02 19:02:14 +0000

    main.py

--- main.py
+++ main.py
@@ -1,53 +1,46 @@
 import time
-from tabulate import tabulate
 from pynput.mouse import Button, Controller
-from itertools import product, combinations, permutations
-from math import factorial
 import itertools
-import string
+
 
 def objeto(i):
+    print("Objeto: ", num)
+    print("\n")
     if i == 0:
-        mouse.position = (874,773)
+        mouse.position = (884, 775)
         mouse.click(Button.left, 1)
-        time.sleep(0.07)
     elif i == 1:
-        mouse.position = (952,777)
+        mouse.position = (989, 778)
         mouse.click(Button.left, 1)
-        time.sleep(0.07)
     elif i == 2:
         mouse.position = (1023, 776)
         mouse.click(Button.left, 1)
-        time.sleep(0.07)
     else:
         print("Nenhum")
+
+
 mouse = Controller()
 numTotal = int(input("Total de movimentos: "))
 numObjetos = int(input("Total de objetos: "))
 counter = numTotal
 my_list = [0] * numTotal
+listNum = [0] * numObjetos
 time.sleep(2)
+for i in range(0, numObjetos):
+    listNum[i] = i
 
-
-
-print(my_list)
-n = len(my_list)
-k = numObjetos
-def pr(n,k,numTotal):
-    counter = numTotal
-    if n < 1:
-        for i in my_list:
-            #print(i, end="")
-            #print()
-            objeto(i)
+comb = list(itertools.product(listNum, repeat=numTotal))
+result = itertools.chain(comb)
+print(comb)
+for each in result:
+    time.sleep(0.1)
+    for num in each:
         time.sleep(0.1)
-        for i in range(0, numTotal):
-            mouse.position = (1438, 185)
+        print(num)
+        objeto(num)
+    print("\n")
+    time.sleep(0.5)
+    for i in range(0, numTotal):
+            mouse.position = (1304, 182)
             mouse.click(Button.left, 1)
-            time.sleep(0.07)
-
-    else:
-        for i in range(k):
-            my_list[n - 1]=i
-            pr(n-1,k,numTotal)
-pr(n,k,numTotal)
+            time.sleep(0.1)
